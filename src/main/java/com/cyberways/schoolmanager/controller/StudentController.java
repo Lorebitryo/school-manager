6ed64842e24c0d2094d2fe3b85bbb1b7ccb6cde2
Lory <lorebitryo@gmail.com>
2024-05-11 21:33:38 +0200

    Lesson 15: Refactoring GetMapping

--- src/main/java/com/cyberways/schoolmanager/controller/StudentController.java
+++ src/main/java/com/cyberways/schoolmanager/controller/StudentController.java
@@ -3,30 +3,41 @@ package com.cyberways.schoolmanager.controller;
 import com.cyberways.schoolmanager.model.Student;
 import com.cyberways.schoolmanager.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
-    @Autowired
-    private StudentService studentService;
+    private final StudentService studentService;
 
-    @GetMapping("/student/{id}")
-    public String getStudentById(@PathVariable Long id) {
-        return studentService.getStudentById(id).get().toString();
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
-    @PostMapping("/student")
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+
+        Optional<Student> student = studentService.getStudentById(id);
+
+        return student.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
     public String createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
     }
 
-    @PutMapping("/student")
+    @PutMapping
     public String updateStudent(@RequestBody Student student) {
         return studentService.updateStudent(student);
     }
 
-    @DeleteMapping("/student/{id}")
+    @DeleteMapping("/{id}")
     public String deleteStudentById(@PathVariable Long id) {
         return studentService.deleteStudentById(id).toString();
     }
